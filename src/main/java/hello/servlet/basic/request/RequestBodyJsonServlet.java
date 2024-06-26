fd00d7d583d2a188836a9b5d191783c9315c4e2f
DongHyeon Yoon <128972134+donghyeon0619@users.noreply.github.com>
2024-06-26 15:05:16 +0900

    :hammer: HttpServletRequset API메시지 바디 처리 코드 구현

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,44 @@
+package hello.servlet.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+
+/**
+ * http://localhost:8080/request-body-json
+ *
+ * JSON 형식 전송
+ * content-type: application/json
+ * message body: {"username": "hello", "age": 20}
+ *
+ */
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+//        JSON 결과를 파싱해서 사용할 수 있는 객체로 변환하려면 이러한Jackson, Gson 같은 JSON 변환 라이브러리를 추가해서 사용해야 한다.
+        ObjectMapper objectMapper = new ObjectMapper(); // string형식으로 온 json을 객체형식으로 변환해줌
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        System.out.println("helloData.username = " + helloData.getUsername());
+        System.out.println("helloData.age = " + helloData.getAge());
+
+        response.getWriter().write("ok");
+    }
+}
