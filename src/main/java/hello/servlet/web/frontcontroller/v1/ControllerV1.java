17559f6a17cd99057237e9daba299da651c3dee6
DongHyeon Yoon <128972134+donghyeon0619@users.noreply.github.com>
2024-08-14 15:33:35 +0900

    :speech_balloon: 주석추가

--- src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
@@ -6,6 +6,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+/**
+ * 다형성을 활용하기 위한 컨트롤러 인터페이스 구현
+ * */
+
 public interface ControllerV1 {
     void process(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException;
 }
